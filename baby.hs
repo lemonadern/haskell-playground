77627c6806274b0a27713ccb9a8711d969169ab9
lemonadern <taishi.develop@gmail.com>
2023-01-23 21:31:32 +0900

    wip

--- baby.hs
+++ baby.hs
@@ -11,3 +11,5 @@ boomBangs xs = [if x < 10 then "Boom!" else "Bang!" | x <- xs, odd x]
 length' xs = sum [1 | _ <- xs]
 
 extractUppercase st = [x | x <- st, x `elem` ['A' .. 'Z']]
+
+triangles = [(a, b, c) | c <- [1 .. 10], b <- [1 .. c], a <- [1 .. b], a ^ 2 + b ^ 2 == c ^ 2, a + b + c == 24]
