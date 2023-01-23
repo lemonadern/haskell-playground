f12b733bdda63d6cc966ee6ddecd6c2893ccdd7e
lemonadern <taishi.develop@gmail.com>
2023-01-23 21:09:41 +0900

    wip

--- baby.hs
+++ baby.hs
@@ -6,3 +6,8 @@ doubleSmallNumber x = if x > 100 then x else x * 2
 
 doubleSmallNumber' x = succ $ doubleSmallNumber x
 
+boomBangs xs = [if x < 10 then "Boom!" else "Bang!" | x <- xs, odd x]
+
+length' xs = sum [1 | _ <- xs]
+
+extractUppercase st = [x | x <- st, x `elem` ['A' .. 'Z']]
