5c54e0678463799f8ff996a82ccf3004832bf135
lemonadern <taishi.develop@gmail.com>
2023-01-23 22:44:04 +0900

    wip

--- baby.hs
+++ baby.hs
@@ -10,6 +10,13 @@ boomBangs xs = [if x < 10 then "Boom!" else "Bang!" | x <- xs, odd x]
 
 length' xs = sum [1 | _ <- xs]
 
+extractUppercase :: String -> String
 extractUppercase st = [x | x <- st, x `elem` ['A' .. 'Z']]
 
 triangles = [(a, b, c) | c <- [1 .. 10], b <- [1 .. c], a <- [1 .. b], a ^ 2 + b ^ 2 == c ^ 2, a + b + c == 24]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+l = [1..5]
+lengthPlusThreeAsFloat = fromIntegral(length l) + 3.0 
