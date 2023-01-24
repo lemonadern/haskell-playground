a394a82d8c9e7eacabe921ed390687c559b181a2
lemonadern <taishi.develop@gmail.com>
2023-01-24 18:21:49 +0900

    wip

--- baby.hs
+++ baby.hs
@@ -1,3 +1,5 @@
+import Distribution.Compat.CharParsing (CharParsing (char))
+
 doubleMe x = x + x
 
 doubleUs x y = doubleMe x + doubleMe y
@@ -18,5 +20,22 @@ triangles = [(a, b, c) | c <- [1 .. 10], b <- [1 .. c], a <- [1 .. b], a ^ 2 + b
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
 
-l = [1..5]
-lengthPlusThreeAsFloat = fromIntegral(length l) + 3.0 
+l = [1 .. 5]
+
+lengthPlusThreeAsFloat = fromIntegral (length l) + 3.0
+
+lucky :: (Integral a) => a -> String
+lucky 7 = "Luckey Number 7!!!"
+lucky x = "Sorry, you're out of luck..."
+
+factorical :: (Integral a) => a -> a
+factorical 0 = 1
+factorical x = x * factorical (x - 1)
+
+charName :: Char -> String
+charName 'a' = "alpha"
+charName 'b' = "Beta"
+
+addVectors :: (Integral a) => (a, a) -> (a, a) -> (a, a)
+-- addVectors a b = (fst a + fst b, snd a + snd b) こうするのではなく
+addVectors (ax, ay) (bx, by) = (ax + bx, ay + by)
